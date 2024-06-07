447bd9a8a0ee3ae96b9ae791aa60181753ae9519
Vladislav Trunin <96149010+legoushka@users.noreply.github.com>
2024-06-07 22:40:48 +0300

    Create solve.py

---
+++ vka2024/rustics/solve.py
@@ -0,0 +1,501 @@
+temp = []
+for char in range(256):
+    if (
+        14 * char * 31 - 43 * 21
+        | 297 * 71 ^ 71 - 66 - 116 - 198 ^ 141 - 136 ^ 247 ^ 83 - 157 + 228 * 146
+        == -4643
+    ):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    222 + 928
+    ^ 41 - 597 + 673 - 155 * 134 * 155 * char * 986 - 29 - 52 + 130
+    ^ 211 + 991
+    ^ 51 * 135
+    ^ 683
+    ^ 17 - 143
+    == 339647857170
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    90
+    | 388 + 892 ^ 101 + 169 - 142 + 77 ^ 275 * 789 - 37 + 85 * char * 73
+    | 772
+    | 580 ^ 60 * 162
+    == 819198
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    217
+    ^ 170 + 693 - 41
+    ^ 169 + 82 * 178 + 562 - 49 * 28
+    ^ 18 + char + 166
+    == 13407
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    421 - 197 + 3 ^ char * 533 * 581 * 548 * 75 + 634 + 329 * 200 ^ 41
+    | 571
+    | 68 * 734
+    | 509 + 160 ^ 732 - 45
+    == 674560795647
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    117 - 838 * 950 * 173 * 109 - char * 164
+    ^ 60
+    ^ 74 + 66 - 15
+    ^ 492 + 608
+    | 984 * 47 ^ 48 * 117 * 847
+    == -15007339592
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    190 ^ 153
+    | 328 * 33 + 47 * 23
+    | 118 + 136 * 821 - 145 + 14 - 476 + 107 * 174 + 7
+    ^ 39 * 72 * char - 138 * 151
+    | 80
+    == 372727
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    187 - 11 + 844 + 365 - 338 ^ 182 ^ 198 + char ^ 109 ^ 117 + 13
+    == 1382
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    36 * 428 + 71 - 624 * 127 - 416 - 90 * 16 ^ 116 - 73
+    | char ^ 597 - 552 - 129 ^ 70 * 77
+    | 137
+    == -97
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    28 + 163
+    | 710 + 68 ^ 797 ^ 78 * 647
+    | 940 * 489 * char
+    + 866 * 168
+    + 69
+    - 82
+    - 82
+    - 728
+    - 114
+    + 271
+    + 54
+    - 34
+    == 49804799
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    237 - 3 * 434 - 151 + 923 - 94
+    ^ 47
+    ^ 160
+    ^ 351 * 372 - 131
+    ^ 13 - char - 80
+    ^ 165
+    == 130130
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    180 * 115 ^ 16 + 172 + 53 + 44 * 189 + 72 + 1 ^ 187
+    | char * 957 * 674 * 177 ^ 24 ^ 165 * 745 + 561 * 112
+    | 265
+    == 11302762491
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    140 * 158 * 156 ^ 88 + char * 593 - 778 + 425
+    | 40
+    | 57 + 988 ^ 132 * 229 ^ 190 + 403 - 375 * 445 * 846 + 689 * 112
+    == -138970002
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    146 - 881 - 443 - 21
+    ^ char - 231
+    ^ 96 - 98 - 170 + 726 - 612 + 405 + 132
+    ^ 27 * 750
+    == 19160
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    123
+    | 137 ^ 167 ^ 287 - 37 * 184 * char ^ 373 - 65 * 134 ^ 870
+    | 110 * 131
+    | 226
+    | 581 - 161 + 42
+    | 158 + 187
+    | 58
+    | 125
+    == 818687
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    77
+    | 95 + 196 - 506
+    ^ 80 + 53 + 41 - 140 * 26 + 19 - 164 + 518
+    ^ 154 - char * 892 * 122 * 105 - 437 * 66 - 3 - 394
+    == -582780451
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    47 + 114 ^ 29
+    | 31 * char * 98 + 32 + 105
+    | 163 ^ 349
+    | 399 * 996
+    | 46
+    == 432127
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    689 - 142
+    | 715 + 17 * 137
+    | 889 + (char - 1) * 324 + 32 - 120 ^ 68 * 192 + 697
+    == 44015
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    237 * 738 - 157 * 161 * 149 - 155 ^ 58 ^ 132 - 451
+    | 166 ^ 563 * 301 * 159 * 163 * 193 * char ^ 120 * 468
+    == 46620791983471
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    707 + 142 * 149 * 885 * 34
+    ^ 853
+    ^ 124 * 71 * char * 736 - 169 - 274 - 980
+    | 702 ^ 164
+    | 845 * 48
+    | 81 ^ 116 * 138
+    == 924761851
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    623 * 530 + 2 - 114 - 189 * 920 - 107
+    ^ 151 - 111 + 271 - 30 - 99 * 98
+    ^ char
+    | 327 + 249 ^ 61 * 179 ^ 36 * 156 * 150
+    == -132353
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    26 ^ 25 ^ 67 + 18 - 145 ^ 295 * char - 383 ^ 360 + 55
+    | 554 - 155 ^ 69 ^ 97
+    == -31813
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    538 - 5 * 324 * char
+    ^ 765
+    ^ 237
+    ^ 46 - 739
+    ^ 798 - 478 - 148 * 285 - 68 - 791
+    ^ 427 + 77 + 460 - 282 - 698
+    == 192868
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    186 * 104 * 78 * char * 191
+    | 685 + 163 - 268 + 512 * 16 + 316 ^ 831 + 3 * 363 - 881 - 446 + 531 * 861
+    == 21038038968
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    824 ^ 285 + 961 + 55 - 193 ^ 580 + 595 - 127 * 134 * char
+    | 57 * 178 - 95 - 10 - 822 ^ 832 ^ 155
+    == -890887
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    930 ^ 123
+    | 655 - 101 + 186
+    ^ 117 * 504 - 58 + char * 912 * 177 + 21 + 912 + 107
+    | 773
+    | 818 - 431 - 121
+    == 15395839
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    226 ^ 162 ^ 21
+    | 132 - 195 + 609 ^ 166 + 194 ^ 146 * 565 + char * 377
+    | 224 + 132
+    | 121
+    | 41 ^ 127 + 199 - 74
+    == 103421
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    821 - 94 ^ 111 * 917 * char + 86 * 529 - 107 * 420 ^ 920 + 302
+    | 733
+    | 46
+    | 165
+    | 712 * 170 + 371
+    == 4971519
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    134 - 141
+    ^ 384 * 767 - 567 - 466 * 838 * char * 24 * 28
+    ^ 734 * 163 + 739
+    | 145
+    == 24929652381
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    18 + 251 - 158 - 82 - 42 - 126 - 56 + 851 * 957
+    | 25 * 497 * 32 - 522
+    | char ^ 781 * 132 * 176
+    == 18808766
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    538 - 3 + 200 - 25
+    | 271
+    | 406 * 289 ^ 123 - 551 + 737
+    | char * 179 * 196 * 43 * 660
+    == 51775663087
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    137
+    | char * 833 + 959 ^ 145 - 136 * 565 * 708 * 234 + 585 * 362 * 139
+    | 154
+    == -12700848193
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    169 + 68
+    | 154
+    ^ 82
+    ^ 865 - 509 + char * 124 * 740 * 875 + 30 - 58 * 96 * 133
+    | 104
+    == 8590289919
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    945 ^ 65 - 596 + 861 * 55 * 151 + 672 + 142 + char * 353
+    | 367
+    | 171 + 895 - 45 - 147
+    == 7168879
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    965 ^ 967 - 749 + 157
+    | 0 - 611 + 705
+    | 63
+    | 623 + 114 * 194 * char ^ 97
+    | 93
+    == 2434047
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    422 + 309 + 999
+    | 520 * 437 * 86 ^ 500 + 33
+    | 159 ^ 96 ^ 13 - 128 * char * 897 * 817 - 926
+    | 41 * 756
+    | 120 + 116 - 93
+    == -5046042625
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    62 - 980 + 29 - 4 + 107 * char * 679 * 429
+    ^ 138 * 527 + 111 * 182 + 51 + 962
+    | 44 ^ 58 + 175 - 90 + 149
+    == 2961029039
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    109
+    | 76
+    | 106 * char * 838 ^ 109 * 95 * 38 + 140 + 72 ^ 33
+    | 959 + 727 * 609 * 172 * 97 ^ 137 ^ 83 + 152
+    == 7386725375
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    182
+    | 872 + 196
+    ^ 187 * char * 571
+    ^ 174 * 809 * 693 * 86 * 743 - 112 * 177
+    == 6233296188855
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    951 - 717 - 248
+    ^ 519
+    ^ 562 * 112 - 32 - char - 56 * 139 * 147 * 366 * 49
+    ^ 165 * 118 - 72
+    == 20520862543
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    657 * 83 * 736 - 29 * 207 * 146 - char
+    ^ 440 * 628
+    ^ 265
+    ^ 719
+    ^ 351 * 427 + 120 - 141
+    | 131 * char - 183
+    == 38895095
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    173 * 478 - 28 * char
+    ^ 914 - 33 * 11 * 124 * 123 - 176 * 856 * 153 - 159
+    == -28642755
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    131 * 250 * 156 + 28 * 377 * 136 * char * 177
+    | 991 + 326 * 166 + 101 - 944 ^ 47 ^ 699 ^ 70 + 710
+    == 27956605800
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    952 * char
+    ^ 316 - 379 - 82 + 11 * 48
+    ^ 106 * 927 * 607 - 359 - 433
+    ^ 63
+    | 57
+    == 59707451
+):
+        temp.append(chr(char))
+print(temp)
+temp = []
+for char in range(256):
+    if (
+    120
+    | 4 + 809 + 108 + 606 + 472 + 311 * 434 * 143 * char * 417 - 827
+    | 86 - 67 - 169 - 253 ^ 120 - 872
+    == 1006079326207
+):
+        temp.append(chr(char))
+print(temp)
